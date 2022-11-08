54142398cec187d4c16077328d9788ad98dddb7e
Isaiah Henson <Devpuddles@gmail.com>
2022-11-08 00:10:55 -0500

    creting react componants

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root"))
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
+
+function MainContent(){
+    return (
+        ReactDOM.render(<h1>This is my main content</h1>, document.getElementById('mainContent'))
+    )
+}
+
+MainContent()
+
